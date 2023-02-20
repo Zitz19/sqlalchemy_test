6e12ac93b4fe2281cdbda8e40b22a2760cf60610
Zitz19 <alex09102001@gmail.com>
2023-02-21 00:24:49 +0300

    new repo test 3

--- app/quiz/views.py
+++ app/quiz/views.py
@@ -1,3 +1,5 @@
+import aiohttp_session
+from aiohttp.web_exceptions import HTTPUnauthorized, HTTPForbidden, HTTPConflict, HTTPBadRequest, HTTPNotFound
 from aiohttp_apispec import querystring_schema, request_schema, response_schema
 from app.quiz.schemes import (
     ListQuestionSchema,
@@ -8,30 +10,91 @@ from app.quiz.schemes import (
 )
 from app.web.app import View
 from app.web.mixins import AuthRequiredMixin
+from app.web.utils import json_response
 
 
 class ThemeAddView(AuthRequiredMixin, View):
     @request_schema(ThemeSchema)
     @response_schema(ThemeSchema)
     async def post(self):
-        raise NotImplemented
+        @request_schema(ThemeSchema)
+        @response_schema(ThemeSchema)
+        async def post(self):
+            session = await aiohttp_session.get_session(self.request)
+            if not session:
+                raise HTTPUnauthorized
+            user = await self.store.admins.get_by_email(session['admin']['email'])
+            if not user or session['admin']['email'] != user.email:
+                raise HTTPForbidden
+            else:
+                title = self.data['title']
+                theme = await self.store.quizzes.get_theme_by_title(title)
+                if theme:
+                    raise HTTPConflict
+                else:
+                    theme = await self.store.quizzes.create_theme(title=title)
+                    return json_response(data=ThemeSchema().dump(theme))
 
 
 class ThemeListView(AuthRequiredMixin, View):
     @response_schema(ThemeListSchema)
     async def get(self):
-        raise NotImplemented
+        session = await aiohttp_session.get_session(self.request)
+        if not session:
+            raise HTTPUnauthorized
+        user = await self.store.admins.get_by_email(session['admin']['email'])
+        if not user or session['admin']['email'] != user.email:
+            raise HTTPForbidden
+        else:
+            themes = await self.store.quizzes.list_themes()
+            raw_themes = [ThemeSchema().dump(theme) for theme in themes]
+            return json_response(data={'themes': raw_themes})
 
 
 class QuestionAddView(AuthRequiredMixin, View):
     @request_schema(QuestionSchema)
     @response_schema(QuestionSchema)
     async def post(self):
-        raise NotImplemented
+        session = await aiohttp_session.get_session(self.request)
+        if not session:
+            raise HTTPUnauthorized
+        user = await self.store.admins.get_by_email(session['admin']['email'])
+        if not user or session['admin']['email'] != user.email:
+            raise HTTPForbidden
+        else:
+            title = self.data['title']
+            theme_id = self.data['theme_id']
+            answers = self.data['answers']
+            only_answer: bool = False
+            for answer in answers:
+                if answer['is_correct']:
+                    if only_answer:
+                        raise HTTPBadRequest
+                    only_answer = True
+            if not only_answer:
+                raise HTTPBadRequest
+            if len(answers) == 1:
+                raise HTTPBadRequest
+            if not await self.store.quizzes.get_theme_by_id(theme_id):
+                raise HTTPNotFound
+            if await self.store.quizzes.get_question_by_title(title):
+                raise HTTPConflict
+            question = await self.store.quizzes.create_question(title, theme_id, answers)
+            return json_response(data=({'id': question.id} | self.data))
 
 
 class QuestionListView(AuthRequiredMixin, View):
     @querystring_schema(ThemeIdSchema)
     @response_schema(ListQuestionSchema)
     async def get(self):
-        raise NotImplemented
+        session = await aiohttp_session.get_session(self.request)
+        if not session:
+            raise HTTPUnauthorized
+        user = await self.store.admins.get_by_email(session['admin']['email'])
+        if not user or session['admin']['email'] != user.email:
+            raise HTTPForbidden
+        else:
+            theme_id = self.request.query.get('theme_id')
+            questions = await self.store.quizzes.list_questions(theme_id)
+            raw_questions = [QuestionSchema().dump(question) for question in questions]
+            return json_response(data={'questions': raw_questions})
--- app/store/quiz/accessor.py
+++ app/store/quiz/accessor.py
@@ -1,36 +1,164 @@
+from sqlalchemy import select
+from sqlalchemy.engine import ChunkedIteratorResult
+
 from app.base.base_accessor import BaseAccessor
 from app.quiz.models import (
     Answer,
     Question,
-    Theme,
+    Theme, ThemeModel, AnswerModel, QuestionModel,
 )
 
 
 class QuizAccessor(BaseAccessor):
     async def create_theme(self, title: str) -> Theme:
-        raise NotImplemented
+        # query = insert(ThemeModel).values(title=title)
+        async with self.app.database.session.begin() as session:
+            # await session.execute(query)
+            session.add(ThemeModel(
+                title=title
+            ))
+            await session.commit()
+            res: ChunkedIteratorResult = await session.execute(
+                select(ThemeModel).where(ThemeModel.title == title)
+            )
+            raw_res = res.scalars().all()
+            await session.commit()
+            # await session.commit()
+        return Theme(
+            id=raw_res[0].id,
+            title=raw_res[0].title
+        )
 
     async def get_theme_by_title(self, title: str) -> Theme | None:
-        raise NotImplemented
+        query = select(ThemeModel).where(ThemeModel.title == title)
+        async with self.app.database.session.begin() as session:
+            res: ChunkedIteratorResult = await session.execute(query)
+            raw_res = res.scalars().all()
+            await session.commit()
+        if len(raw_res) == 0:
+            return None
+        else:
+            return Theme(
+                id=raw_res[0].id,
+                title=raw_res[0].title
+            )
 
     async def get_theme_by_id(self, id_: int) -> Theme | None:
-        raise NotImplemented
+        query = select(ThemeModel).where(ThemeModel.id == id_)
+        async with self.app.database.session.begin() as session:
+            res: ChunkedIteratorResult = await session.execute(query)
+            raw_res = res.scalars().all()
+            await session.commit()
+        if len(raw_res) == 0:
+            return None
+        else:
+            return Theme(
+                id=raw_res[0].id,
+                title=raw_res[0].title
+            )
 
     async def list_themes(self) -> list[Theme]:
-        raise NotImplemented
+        themes = []
+        query = select(ThemeModel)
+        async with self.app.database.session.begin() as session:
+            res: ChunkedIteratorResult = await session.execute(query)
+            raw_res = res.scalars().all()
+            await session.commit()
+        for raw_theme in raw_res:
+            themes += [Theme(raw_theme.id, raw_theme.title)]
+        return themes
 
     async def create_answers(
-        self, question_id: int, answers: list[Answer]
+            self, question_id: int, answers: list[Answer]
     ) -> list[Answer]:
-        raise NotImplemented
+        for answer in answers:
+            # query = insert(AnswerModel).values(
+            #    is_correct=answer.is_correct,
+            #    title=answer.title,
+            #    question_id=question_id
+            # )
+            async with self.app.database.session.begin() as session:
+                # await session.execute(query)
+                session.add(AnswerModel(
+                    is_correct=answer.is_correct,
+                    title=answer.title,
+                    question_id=question_id
+                ))
+                await session.commit()
+        return answers
 
     async def create_question(
         self, title: str, theme_id: int, answers: list[Answer]
     ) -> Question:
-        raise NotImplemented
+        async with self.app.database.session.begin() as session:
+            # await session.execute(query)
+            session.add(QuestionModel(
+                title=title,
+                theme_id=theme_id
+            ))
+            await session.commit()
+            res: ChunkedIteratorResult = await session.execute(
+                select(QuestionModel).where(QuestionModel.title == title)
+            )
+            raw_res = res.scalars().all()
+            await session.commit()
+        raw_answers = await self.create_answers(raw_res[0].id, answers)
+        return Question(
+            id=raw_res[0].id,
+            theme_id=theme_id,
+            title=title,
+            answers=raw_answers
+        )
 
     async def get_question_by_title(self, title: str) -> Question | None:
-        raise NotImplemented
+        query = select(QuestionModel).where(QuestionModel.title == title)
+        async with self.app.database.session.begin() as session:
+            res: ChunkedIteratorResult = await session.execute(query)
+            raw_res_q = res.scalars().all()
+            await session.commit()
+        if len(raw_res_q) == 0:
+            return None
+        else:
+            query = select(AnswerModel). \
+                where(AnswerModel.question_id == raw_res_q[0].id)
+            res: ChunkedIteratorResult = await session.execute(query)
+            raw_res_a = res.scalars().all()
+            await session.commit()
+            question = Question(
+                id=raw_res_q[0].id,
+                title=raw_res_q[0].title,
+                theme_id=raw_res_q[0].theme_id,
+                answers=[Answer(title=x.title, is_correct=x.is_correct)
+                         for x in raw_res_a]
+            )
+            return question
 
     async def list_questions(self, theme_id: int | None = None) -> list[Question]:
-        raise NotImplemented
+        if not theme_id:
+            query = select(QuestionModel)
+        else:
+            query = select(QuestionModel).where(QuestionModel.theme_id == int(theme_id))
+        questions = []
+        async with self.app.database.session.begin() as session:
+            res: ChunkedIteratorResult = await session.execute(query)
+            raw_res_q = res.scalars().all()
+            await session.commit()
+        for raw_q in raw_res_q:
+            raw_answers = []
+            query = select(AnswerModel).where(AnswerModel.question_id == raw_q.id)
+            async with self.app.database.session.begin() as session:
+                res: ChunkedIteratorResult = await session.execute(query)
+                raw_res_a = res.scalars().all()
+                await session.commit()
+            for raw_a in raw_res_a:
+                raw_answers += [Answer(
+                    title=raw_a.title,
+                    is_correct=raw_a.is_correct
+                )]
+            questions += [Question(
+                id=raw_q.id,
+                title=raw_q.title,
+                theme_id=raw_q.theme_id,
+                answers=raw_answers
+            )]
+        return questions
